cd476d0ada591f4b7a2151581380f3442ab52528
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-04-01 13:38:24 +0800

    Update main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,4 +3,21 @@
 
 int lookup_speeding_fine(int over)
 {
-  
+  #include<iostream>
+#include<string>
+
+int lookup_speeding_fine(int over)
+{
+ if (over <= 0)
+  return 0;
+ else if (over > 0 && over < 10)
+  return 180;
+ else if (over >= 10 && over < 20)
+  return 406;
+ else if (over >= 20 && over < 30)
+  return 825;
+ else if (over > 30 && over < 45)
+  return 1500;
+ else 
+  return 1690;
+}
