87ebde90c385b7044be44b7fd52b7085aaa14dff
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-04-01 13:35:59 +0800

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
-using namespace std;
 
-
-int* shift_right(int* vals, int len,int amout);
-
-
-int main()
+int* shift_right(int* vals, int len, int amout)
 {
- int vals[] = {5,4,3,2,1,1 };
- int * s = shift_right(vals, 6,3);
- for (int i = 0; i < 6; i++)
- {
-  cout << s[i];
-
+ int* tmp = new int[len];
+ int temp;
+ for (int i = 0; i < amout; i++) {
+  temp = vals[len - 1];
+  for (int j = len - 1; j > 0; j--) {
+   vals[j] = vals[j - 1];
+  }
+  vals[0] = temp;
  }
-
+ for (int i = 0; i < amout; i++) {
+  vals[i] = 0;
+ }
+ //
+ for (int i = 0; i < len; i++)
+ {
+  tmp[i] = vals[i];
+ }
+ return tmp;
 }
