569dd448a3f31492c7f7fa3ace8eded2a3d72ba8
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-04-01 13:47:53 +0800

    Update function-2-1.cpp

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 #include<string>
 
-int lookup_speeding_fine(int over)
+int lookup_speeding_fine(int over);
 
- int main()
- {
-   int over = 0;
-   std::cout << "my speeding fine is : " << lookup_speeding_fine(over) << std::endl;
-   over = 10;
-   std::cout << "my speeding fine is : " << lookup_speeding_fine(over) << std::endl;
-   over = 21;
-   std::cout << "my speeding fine is : " << lookup_speeding_fine(over) << std::endl;
-   over = 50;
-   std::cout << "my speeding fine is : " << lookup_speeding_fine(over) << std::endl;
-   return 0;
- }
+int main()
+{
+ int over = 0;
+ std::cout << "my speeding fine is: " << lookup_speeding_fine(over) << std::endl;
+  over = 10;
+ std::cout << "my speeding fine is: " << lookup_speeding_fine(over) << std::endl;
+  over = 21;
+ std::cout << "my speeding fine is: " << lookup_speeding_fine(over) << std::endl;
+  over = 50;
+ std::cout << "my speeding fine is: " << lookup_speeding_fine(over) << std::endl;
+ return 0;
+}
