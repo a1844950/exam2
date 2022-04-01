a3c67f08d3e6a07ce12ca60936bd3784431fa287
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-04-01 13:29:06 +0800

    Update function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+ 
+int count_number_in_range(int vals[], int length, int bottom, int top)
+{
+  ///counter
+  int count = 0;
+  
+  for (int i = 0; i < length; i++)
+  {
+    ///Top - Botton
+    if (vals[i] <=top && >=botton)
+      count++;
+  }
+  return count;
+}
