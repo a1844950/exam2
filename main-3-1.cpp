2b99412a5a5fa40e92247450b9546f8462870d22
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-04-01 13:24:42 +0800

    Update main-3-1.cpp

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int count_number_in_range(int vals[],int length, int botton, int top);
+int main()
+{
+  int vals[] = { 1,7,3,4,6,5,2 };
+  int res = count_number_in_range(vals,7,3,6);
+  cout << res;
+  
+  return 0;
+}
